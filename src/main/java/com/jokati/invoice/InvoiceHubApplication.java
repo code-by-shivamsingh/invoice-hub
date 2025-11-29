2f943569810fc7b66d08398993b014cf8a925959
satender55tech <satender.gautam@fiftyfivetech.io>
2025-11-29 23:23:58 +0530

    initial commmmit

---
+++ src/main/java/com/jokati/invoice/InvoiceHubApplication.java
@@ -0,0 +1,13 @@
+package com.jokati.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokati/invoice/InvoiceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokati.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
