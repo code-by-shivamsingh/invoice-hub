713d97a445f6688853988f32d5ff170e5d92664d
code-by-vivek <vivek458716@gmail.com>
2026-01-14 13:51:02 +0530

    Add missing StatusInfo model

---
+++ src/main/java/com/jokati/invoice/model/StatusInfo.java
@@ -0,0 +1,21 @@
+package com.jokati.invoice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class StatusInfo {
+	
+	private String label;
+	private String color;
+
+}
