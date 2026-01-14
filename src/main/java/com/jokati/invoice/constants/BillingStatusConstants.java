4c6b85af5ac0a8e78cb66556ffd98c403cba11d7
code-by-vivek <vivek458716@gmail.com>
2026-01-14 13:55:30 +0530

    constants

---
+++ src/main/java/com/jokati/invoice/constants/BillingStatusConstants.java
@@ -0,0 +1,17 @@
+package com.jokati.invoice.constants;
+
+public final class BillingStatusConstants {
+
+    private BillingStatusConstants() {}
+
+    
+    public static final String CORRECT_BILLING = "Correct billing";
+    public static final String INCORRECT_BILLING = "Incorrect billing";
+    public static final String ACCEPTED = "Accepted";
+    public static final String TOLERANCE_ACCEPTED = "Tolerance accepted";
+
+    
+    public static final String COLOR_SUCCESS = "#28a745";
+    public static final String COLOR_ERROR = "#dc3545";
+    public static final String COLOR_WARNING = "#ffc107";
+}
